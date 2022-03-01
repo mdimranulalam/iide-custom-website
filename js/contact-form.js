9290ac2648da52d902652c00f364864e1baa9eab
iamimran <64079533+mdimranulalam@users.noreply.github.com>
2022-03-02 04:54:12 +0600

    submission return feedback added

--- js/contact-form.js
+++ js/contact-form.js
@@ -13,6 +13,5 @@
     form2.addEventListener('submit', e => {
         e.preventDefault()
         fetch(scriptURL, { method: 'POST', body: new FormData(form2) })
-            .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
-            .catch(error => console.error('Error!', error.message))
+            
     })
