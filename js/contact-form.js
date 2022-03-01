ac2f9e4966f6188f5ce80d0a8f78aa6bdf8e80da
iamimran <64079533+mdimranulalam@users.noreply.github.com>
2022-03-02 03:00:07 +0600

    contact form added 100% working

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,10 +1,18 @@
     const scriptURL = 'https://script.google.com/macros/s/AKfycbyqwD_J53kPwCkP36biRUYyMPBDD4j20qO0kOPHqu68GwXxOaQutmvwUguLrG6A1JjEZA/exec'
 
     const form = document.forms['google-sheet']
+    const form2 = document.forms['google-sheet-2']
 
     form.addEventListener('submit', e => {
         e.preventDefault()
         fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+            .catch(error => console.error('Error!', error.message))
+    })
+    
+    form2.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form2) })
             .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
             .catch(error => console.error('Error!', error.message))
     })
